f1c6f76b158cc28fccd4cadd241232a3f078ba81
justericli <eli@hamilton.edu>
2023-07-07 16:54:05 -0700

    moving

--- src/components/User_dashboard.jsx
+++ src/components/User_dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import UserPool from "../User_Userpool.jsx"; // adjust the import path to where your User_Userpool.jsx file is located
 import { Auth } from "aws-amplify";
+import "./User_dashboard.css";
 
 const User_dashboard = () => {
   const [givenName, setGivenName] = useState("");
@@ -29,17 +30,49 @@ const User_dashboard = () => {
     if (window.FB) {
       window.FB.logout();
     }
-
-    // Redirect the user to the login page
-    window.location.href = "/User_login";
   }
+
+  useEffect(() => {
+    // Get DOM elements
+    const addFriendsButton = document.getElementById("addFriendsButton");
+    const modal = document.getElementById("modal");
+    const closeModalButton = document.getElementById("closeModalButton");
+
+    // Event listener for "Add Friends" button click
+    addFriendsButton.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+
+    // Event listener for "Close" button click
+    closeModalButton.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  }, []);
 
   return (
     <div>
-      Hi, {givenName}
-      <button onClick={handleLogout}>Logout</button>{" "}
-      {/* Button to trigger logout */}
-      <button>Add friends</button>
+      <button id="addFriendsButton">Add Friends</button>
+
+      <div id="modal" className="modal">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h2>Add Friends</h2>
+          </div>
+          <div className="modal-body">
+            <div id="recommendedFriendsSection">
+              <h3>Recommended Friends</h3>
+              {/* Place recommended friends here */}
+            </div>
+            <div id="friendsSection">
+              <h3>Friends</h3>
+              {/* Place friends here */}
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button id="closeModalButton">Close</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
